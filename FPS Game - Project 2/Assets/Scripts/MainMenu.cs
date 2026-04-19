443da89104a8b9be8bd60b4187c1a486a0d344e4
gabrielfawaz26 <gabriel.fawaz01@gmail.com>
2026-04-19 20:11:25 +0300

    Phase 3

--- FPS Game - Project 2/Assets/Scripts/MainMenu.cs
+++ FPS Game - Project 2/Assets/Scripts/MainMenu.cs
@@ -3,8 +3,52 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Panels")]
+    public GameObject infoPanel;
+    public GameObject pausePanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("Map");
+    }
+
+    public void OpenInfo()
+    {
+        infoPanel.SetActive(true);
+    }
+
+    public void CloseInfo()
+    {
+        infoPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
